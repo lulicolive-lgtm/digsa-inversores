0f4afacbb64aea7dd6f417630f1992e9011a1544
Luana Sarfatis <luanasarfatis@MacBook-Air-de-Luana-2.local>
2026-05-13 07:05:32 -0300

    Email: enviar desde contacto@digsa.es via Resend

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,7 +3,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function enviarEmail({ to, subject, html }) {
   const { data, error } = await resend.emails.send({
-    from: 'DIGSA España <onboarding@resend.dev>',
+    from: 'DIGSA España <contacto@digsa.es>',
     to,
     subject,
     html
